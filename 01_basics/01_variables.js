b9e8e1dbb7b24228a262640ddf1c3e404f87af1e
lokendrasinha <195967462+lokendrasinha@users.noreply.github.com>
2025-06-21 18:11:51 +0000

    newfile

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,6 +1,17 @@
 const accountId = 144553;
 let accountEmail = "lokendrasinha2003@gmail.com";
 var accountPassword = "123";
-// accountCity = "Jaipur";
+let accountCity = "Jaipur";
+let accountState;
 
-console.log(accountId)
+accountEmail = "rajat@gmail.com";
+accountPassword = "456";
+
+console.log(accountId);
+
+/*
+preferred not to use var because of issue in block scope and functional scope
+*/
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,29 @@
+"use strict"; //treat all code as newer version
+
+// alert(3+3)   we are using nodejs , not browser
+
+// console.log(3 + 3)
+console.log("Hello")
+
+// code readability should be high
+
+
+let name = "rajat"
+let age=22
+let isLoggedIn = false
+
+// number => 2 to the power 53
+
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value for empty value
+// undefined => value not assigned
+// symbol => used oftenly in react for uniqueness of component
+
+
+// object
+
+console.log(typeof null);
+console.log(typeof undefined);
+
