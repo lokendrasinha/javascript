60bcd40e263cac4726be94b7e41b3d9dbd669530
lokendrasinha <195967462+lokendrasinha@users.noreply.github.com>
2025-06-21 19:00:22 +0000

    code updated

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,18 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1)
+
+
+// console.log("2">1);
+// console.log("02" > 1);
+// avoid this type of comparision
+// console.log(null>0)
+// console.log(null==0)
+// console.log(null>=0)           // convert null to 0
+
+// in js <= >= works different than ==
+
+// === strict check
+
+console.log("2"==="2")   // also check datatype
+
