50e2c0105565937eb8ffc0d675509b152feef152
lokendrasinha <195967462+lokendrasinha@users.noreply.github.com>
2025-06-21 18:28:51 +0000

    added new file

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,28 @@
+// let score = "true"
+
+// console.log(typeof score);
+
+// let valueInNumber = Number(score)
+
+// console.log(valueInNumber);
+
+// "33" can be converted to converted
+// "33abc" => NaN
+// true=> 1 false=>0
+
+let isLoggedIn = "rajat"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+// console.log( booleanIsLoggedIn)
+
+// 1=> true; 0=> false
+// "" => false
+// "rajat" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
+
