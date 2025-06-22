04fa7e5960925c985f2879b3f2ed14e5b35a8b18
lokendrasinha <195967462+lokendrasinha@users.noreply.github.com>
2025-06-22 19:29:26 +0000

    nums and maths added

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,38 @@
+const score = 400
+
+const balance = new Number(100)
+// console.log(score)
+// console.log(balance)
+// console.log(typeof balance)
+// console.log(typeof score)
+
+// console.log( balance.toFixed(2))  //precision value upto 2 after decimal 100.00
+
+const otherNumber = 123.8966
+
+// console.log(otherNumber.toPrecision(4))
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN')); //apply commas according to indian number system to the number
+
+// ===============MATHS ====================
+
+// console.log(Math)
+// console.log(Math.abs(-44))
+// console.log(Math.round(4.5)) //it roundoff by default as normal
+// console.log(Math.ceil(4.2)) //choose upper value
+// console.log(Math.floor(4.6)) //choose lower value
+// console.log(Math.min(4,3,2,1,-1))
+// console.log(Math.max(4,3,2,1,-1))
+
+// console.log(Math.random())
+// console.log((Math.random()*10)+1)
+// console.log((Math.random()*10)+1)
+
+const min = 10
+const max = 20
+console.log(Math.floor(Math.random() * (max - min + 1))+min)
+/*The above snippet generate random int value between min and max */
+
+
+
