723956d6e6a42799c8f6726b68070c8eb75747bf
lokendrasinha <195967462+lokendrasinha@users.noreply.github.com>
2025-06-28 18:32:13 +0000

    01_functions.js updated

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -33,5 +33,29 @@ function loginUserMessage(username = "sam"){
     return `${username}, just logged in`
 }
 // console.log(loginUserMessage("Rajat"))
-console.log(loginUserMessage())  //if we do not pass any argument it takes value of parameter as undefined
+// console.log(loginUserMessage())  //if we do not pass any argument it takes value of parameter as undefined
 
+function calculateCartPrice(val1,val2,...num1){  //... is called as rest operator or spread operator
+    return num1
+}
+// rest or spread is used to take multiple argument for one paramenter in form of array
+// console.log(calculateCartPrice(200,400,500,600))
+
+
+const user = {
+    username:"Rajat",
+    price:199
+}
+
+function handleObject(anyObject){
+    console.log(`Username is ${anyObject.username} and price is ${anyObject.price}`)
+}
+handleObject(user)
+
+const mynewArray = [200,400,500,600]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+
+}
+console.log(returnSecondValue(mynewArray))
