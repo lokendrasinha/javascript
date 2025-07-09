ca3b324edec660543e60e8f77f05fe314e3dc5c9
lokendrasinha <195967462+lokendrasinha@users.noreply.github.com>
2025-07-09 19:11:03 +0000

    02_scopes.js updated

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -21,3 +21,42 @@ if (true){
 // console.log(a)
 // console.log(b)
 // console.log(c)
+
+
+function one(){
+    const username = "hitesh"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+    // console.log(website)   scope of website is only inside two()
+
+    two()    //number of times function called then that no of time call stack is created
+
+}
+
+// one()
+/*
+if(true){
+    const username = "hitesh"
+    if(username==="hitesh"){
+        const website = "youtube"
+        // console.log(username+website);
+    }
+    // console.log(website);   //scope of website is in inner if
+}
+    */
+
+//  +++++++++++++++++++++++++++++++++interesting +++++++++++++++++++++
+
+function addone(value){
+    return num+1
+}
+
+console.log(addone(5))
+
+addTwo(5)    //error
+const addTwo = function(nums){
+    return num+2
+}
