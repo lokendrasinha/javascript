23562255a2fb4ce2d2ea1580c154802820a441fc
lokendrasinha <195967462+lokendrasinha@users.noreply.github.com>
2025-07-09 18:53:07 +0000

    02_scopes.js added

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,23 @@
+
+
+// {}    //is called scope
+// var c=300
+/*let a = 300
+if (true){
+    let a = 10
+    const b = 20
+    console.log("Winner",a)
+    
+}*/
+
+// var has global scope
+// let and const have block scope
+
+/*interview note scope checked through inspection 
+ window of website is different and scope
+ checked in node environment is different*/
+
+
+// console.log(a)
+// console.log(b)
+// console.log(c)
