c23f53d07993e4af95385fed3475d35b73f4a313
lokendrasinha <195967462+lokendrasinha@users.noreply.github.com>
2025-07-26 13:05:38 +0000

    new file added

---
+++ 04_controlFlow/one.js
@@ -0,0 +1,23 @@
+// control flow
+
+// if stratement
+/*const isUserLoggedIn = true
+if (3==2){
+console.log("loggedin")
+
+}
+else{
+    console.log("Not logged in")
+}
+    */
+// <,>,<=,>=,!=,==,===
+// === also checks the type
+
+const temprature = 41
+
+if(temprature===41){
+    console.log("temprature less than 50")
+}
+else{
+    console.log("temprature grster than 50")
+}
