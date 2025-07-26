4d421fa009c7d8ee4328d66b4882290fb4809523
lokendrasinha <195967462+lokendrasinha@users.noreply.github.com>
2025-07-26 13:55:45 +0000

    four.js added

---
+++ 04_controlFlow/four.js
@@ -0,0 +1,21 @@
+const myObject = {
+    js: 'Javascript',
+    cpp:'C++',
+    rn:"ruby",
+    swift:"Swift"
+}
+
+// to iterarate through objects use for in loop
+/*
+for (const key in myObject) {
+console.log(`Key :- ${key}, Value :- ${myObject[key]}`)
+}*/
+/*
+const programming = ["js","rb","py","java","cpp"]
+// here key of arr is its index this is difference in using for in
+for (const key in programming) {
+    console.log(programming[key])
+}*/
+
+
+
